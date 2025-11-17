b6c970da5f43222f73afdd696a2bc49e6634e10c
Joar Paganus <90351038+joarp@users.noreply.github.com>
2025-11-17 14:52:18 +0100

    parameterize notebooks and add more features

--- docs/air-quality/index.md
+++ docs/air-quality/index.md
@@ -2,12 +2,18 @@
 
 ![Hopsworks Logo](../titanic/assets/img/logo.png)
 
+## Comparison of old and new model
+The old model uses default features while the new model uses the default features and lag1, lag2, lag3, and rolling average of the lagged values.
+
+![Comparison](./assets/img/pm25_hindcast_comparison.png)
+
+
 ## Predictions
 
 {% include air-quality.html %}
 
-![Forecast](./assets/img/pm25_forecast_bodo.png)
 ![Forecast](./assets/img/pm25_forecast_moirana.png)
+![Forecast](./assets/img/pm25_forecast_bodo.png)
 
 
 There is also a Python program to interact with the air quality ML system using language (text, voice),
@@ -17,5 +23,5 @@ powered by a [function-calling LLM](https://www.hopsworks.ai/dictionary/function
 
 1-Day Hindcast: Predictions vs Outcomes
 
-![Hindcast](./assets/img/pm25_hindcast_1day_bodo.png)
 ![Hindcast](./assets/img/pm25_hindcast_1day_moirana.png)
+![Hindcast](./assets/img/pm25_hindcast_1day_bodo.png)
--- mlfs/airquality/util.py
+++ mlfs/airquality/util.py
@@ -151,6 +151,16 @@ def get_pm25(aqicn_url: str, city: str, day: datetime.date, AQI_API_KEY: str):
     # Make a GET request to fetch the data from the API
     data = trigger_request(url)
 
+    # if we get 'Unknown station' response then retry with city in url
+    if data['data'] == "Unknown station":
+        url1 = f"https://api.waqi.info/feed/{country}/{street}/?token={AQI_API_KEY}"
+        data = trigger_request(url1)
+
+    if data['data'] == "Unknown station":
+        url2 = f"https://api.waqi.info/feed/{country}/{city}/{street}/?token={AQI_API_KEY}"
+        data = trigger_request(url2)
+
+
     # Check if the API response contains the data
     if data['status'] == 'ok':
         # Extract the air quality data
@@ -275,39 +285,92 @@ def check_file_path(file_path):
     else:
         print(f"File successfully found at the path: {file_path}")
 
-def backfill_predictions_for_monitoring(weather_fg, air_quality_df, monitor_fg, models, city_dict):
-    features_df = weather_fg.read()
-    features_df = features_df.sort_values(by=["city", "date"], ascending=True)
-    features_df = features_df.groupby("city", group_keys=False).apply(lambda g: g.tail(17).head(10))
+def backfill_predictions_for_monitoring(weather_fg, air_quality_fg, monitor_fg, model):
+    weather_df = weather_fg.read()
+    air_quality_df = air_quality_fg.read()
 
-    feature_cols = [
+    # Select relevant columns
+    weather_sel = weather_df[[
         "temperature_2m_mean",
         "precipitation_sum",
         "wind_speed_10m_max",
         "wind_direction_10m_dominant",
-    ]
-    
-    # Make city-specific predictions with the right model
-    for city in city_dict:
-        mask = features_df["city"] == city
-        X_city = features_df.loc[mask, feature_cols]
+        "city",
+        "date",
+    ]]
 
-        y_pred = models[city].predict(X_city)
-        features_df.loc[mask, "predicted_pm25"] = y_pred
+    aq_sel = air_quality_df[[
+        "pm25",
+        "date",
+        "city",
+        "lag1",
+        "lag2",
+        "lag3",
+        "rolling",
+    ]]
 
-    df = pd.merge(
-        features_df,
-        air_quality_df[["city", "date", "pm25"]],
+    features_df = pd.merge(
+        weather_sel,
+        aq_sel,
         on=["city", "date"],
-        how="left",
+        how="inner",
     )
-
-    df["days_before_forecast_day"] = 1.0
-
+    features_df = features_df.sort_values(by=['date'], ascending=True)
+    features_df = features_df.apply(lambda g: g.tail(31).head(30))
+    features_df['predicted_pm25'] = model.predict(features_df[['lag1', 'lag2', 'lag3', 'rolling','temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant']])
+    df = pd.merge(features_df, air_quality_df[['date','city','pm25']], on=["city","date","pm25"], how="left")
+    df['days_before_forecast_day'] = 1
     hindcast_df = df.copy()
-
-    df_to_insert = df.drop(columns=["pm25"])
-
-    monitor_fg.insert(df_to_insert, write_options={"wait_for_job": True})
-
+    df = df.drop(columns=['pm25'])
+    df["predicted_pm25"] = df["predicted_pm25"].astype("float32")
+    monitor_fg.insert(df, write_options={"wait_for_job": True})
+    df
     return hindcast_df
+
+def plot_air_quality_forecast_two_models(city: str, df: pd.DataFrame, file_path: str, mse: float, old_mse: float, r2: float, old_r2: float, hindcast=False):
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    day = pd.to_datetime(df['date']).dt.date
+    # Plot each column separately in matplotlib
+    ax.plot(day, df['predicted_pm25'], label=f'Predicted PM2.5 (MSE={mse}, R2={r2})', color='orange', linewidth=2, marker='o', markersize=5, markerfacecolor='blue')
+
+    # Set the y-axis to a logarithmic scale
+    ax.set_yscale('log')
+    ax.set_yticks([0, 10, 25, 50, 100, 250, 500])
+    ax.get_yaxis().set_major_formatter(plt.ScalarFormatter())
+    ax.set_ylim(bottom=1)
+
+    # Set the labels and title
+    ax.set_xlabel('Date')
+    ax.set_title(f"PM2.5 Predicted (Logarithmic Scale) for {city}")
+    ax.set_ylabel('PM2.5')
+
+    colors = ['green', 'yellow', 'orange', 'red', 'purple', 'darkred']
+    labels = ['Good', 'Moderate', 'Unhealthy for Some', 'Unhealthy', 'Very Unhealthy', 'Hazardous']
+    ranges = [(0, 49), (50, 99), (100, 149), (150, 199), (200, 299), (300, 500)]
+    for color, (start, end) in zip(colors, ranges):
+        ax.axhspan(start, end, color=color, alpha=0.3)
+
+    # Add a legend for the different Air Quality Categories
+    patches = [Patch(color=colors[i], label=f"{labels[i]}: {ranges[i][0]}-{ranges[i][1]}") for i in range(len(colors))]
+    legend1 = ax.legend(handles=patches, loc='upper right', title="Air Quality Categories", fontsize='x-small')
+
+    # Aim for ~10 annotated values on x-axis, will work for both forecasts ans hindcasts
+    if len(df.index) > 11:
+        every_x_tick = len(df.index) / 10
+        ax.xaxis.set_major_locator(MultipleLocator(every_x_tick))
+
+    plt.xticks(rotation=45)
+
+    if hindcast == True:
+        ax.plot(day, df['pm25'], label='Actual PM2.5', color='black', linewidth=2, marker='^', markersize=5, markerfacecolor='grey')
+        ax.plot(day, df['predicted_old_pm25'], label=f'Old Predicted PM2.5 (MSE={old_mse}, R2={old_r2})', color='red', linewidth=2, marker='^', markersize=5, markerfacecolor='grey')
+        legend2 = ax.legend(loc='upper left', fontsize='x-small')
+        ax.add_artist(legend1)
+
+    # Ensure everything is laid out neatly
+    plt.tight_layout()
+
+    # # Save the figure, overwriting any existing file with the same name
+    plt.savefig(file_path)
+    return plt
